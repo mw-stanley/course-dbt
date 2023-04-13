3997488387b606b23ef23b2a879201472153ca24
mw-stanley <fang.xianfu@gmail.com>
2023-04-13 13:53:53 -0700

    Remove spurious aliases

--- greenery/models/staging/postgres/stg_postgres__orders.sql
+++ greenery/models/staging/postgres/stg_postgres__orders.sql
@@ -8,14 +8,14 @@ with source as (
         , user_id as user_uuid
         , promo_id as promo_name
         , address_id as shipping_address_uuid
-        , created_at as created_at
+        , created_at
         , order_cost as order_cost_usd --I will assume everything is in USD
         , shipping_cost as shipping_cost_usd 
         , order_total as order_total_usd
         , tracking_id
         , shipping_service
-        , estimated_delivery_at as estimated_delivery_at       
-        , delivered_at as delivered_at
+        , estimated_delivery_at       
+        , delivered_at
         , status
     from source
 )
